a702d95f565ffaefd77198876b2bc041a2f92b25
viet <vietnetk123@gmail.com>
2022-05-30 11:46:32 +0700

    64. Dung khung layout mac dinh

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import styles from './SideBar.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function SideBar(props) {
-    return <div>SideBar</div>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>SideBar</h2>
+        </aside>
+    );
 }
 
 export default SideBar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,18 +1,22 @@
+import classNames from 'classnames/bind';
 import React from 'react';
 import Header from '~/components/Layout/components/Header';
+import styles from './DefaultLayout.module.scss';
 import SideBar from './SideBar';
 
+const cx = classNames.bind(styles);
+
 function DefaultLayout(props) {
-    const {children} = props
+    const { children } = props;
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className='container'>
+            <div className={cx('container')}>
                 <SideBar />
-                <div className='content'>{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
-    )
+    );
 }
 
 export default DefaultLayout;
